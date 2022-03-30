b8b515d306d1266bf928bb9e03569f787344b51a
Dominic Owens <ddgowens@hotmail.co.uk>
2022-03-30 14:38:09 -0400

    Initial commit

---
+++ alignR.R
@@ -0,0 +1,195 @@
+
+alignR <- function(base="C:/Users/dowens/OneDrive/Postdoc/Projects/GID4/Paper/Bioinformatics/alignR/",
+                   uniprot_IDs = c("P04637","P02340","P10361"),
+                   seqs = c("MEEPQSDPSV","MTAMEESQSD","MEDSQSDMSI"),
+                   n_seqs = c("P53_HUMAN","P53_MOUSE", "P53_RAT"),
+                   n = "alignR_p53"){
+  
+  ## function for aligning two or more amino acid sequences in R
+  ## uses biostrings and msa packages to do the alignment
+  ## will save a pdf of the alignment if tinytex is installed and working
+  
+  ## inputs
+  
+  # base = character vector of the route directory to save output eg "/path/to/route/directory/"
+  # uniprot_IDs = a character vector of uniprot IDs to align eg c("P04637","P02340","P10361") # leave it blank if you are manually inputting the sequences OR
+  # seqs = character vector of custom amino acid sequences to align eg c("MEEPQSDPSV","MTAMEESQSD","MEDSQSDMSI") ## goofd for aligning shorter peptides or other sequences not on uniprot!
+  # n_seqs = character vector of names of amino acid sequences in same order as the sequences or uniprot IDs given eg c("P53_HUMAN","P53_MOUSE", "P53_RAT")
+  # n = prefix name to give the outputs eg "alignR_p53"
+  
+  
+  ## outputs
+  
+  # alignDir = sub directory called "alignR_out/" that contains the alignments, within the base directory specified
+  # alFile = a fasta file of the alignment that can be viewed in other software e.g. snapgene
+  # texFile = a .tex file that can be used to view the alignment manually using texshade in latex
+  # pdfFile = the output pdf alignment made using tinytex. If this file is missing then it is likely that tinytex is not working on your machine.
+  # pidFile = text file containing the percent identify between the sequences. Only generated if number of sequences to align is 2.
+  
+  ## will always try to output a .tex file, and a .fasta file
+  ## if printPDF = TRUE, and **tinytex is working** a pdf of the alignment will also be saved
+  ## and if only two sequences have been aligned then a text file of the percentage identity between them will also be saved
+  
+  
+  
+  ##########################
+  ## package requirements ##
+  ##########################
+  
+  require(Biostrings)
+  require(msa)
+  require(tinytex)
+  
+  ############
+  ## inputs ##
+  ############
+  
+  # route directory
+  #base = "C:/Users/dowens/OneDrive/Postdoc/Projects/GID4/Paper/Bioinformatics/alignR/"
+  
+  # OR the uniprot IDs of the sequences we want to align
+  #uniprot_IDs = c("P04637","P02340","P10361")
+  
+  ## vector of input sequences to align
+  #seqs = c("MEEPQSDPSV","MTAMEESQSD","MEDSQSDMSI")
+  
+  ## vector of names of the sequences (must be in same order as sequences above
+  #n_seqs = c("P53_HUMAN","P53_MOUSE", "P53_RAT")
+  
+  # name of the alignment
+  #n = "alignR_p53"
+  
+  
+  
+  #############
+  ## outputs ##
+  #############
+  
+  alignDir = paste0(base,"alignR_out/")
+  dir.create(alignDir,showWarnings = F)
+  
+  alFile = paste0(alignDir,"alignR_",n,".fasta")
+  texFile = paste0(alignDir,"alignR_",n,".tex")
+  pdfFile = paste0(alignDir,"alignR_",n,".pdf")
+  pidFile = paste0(alignDir,"percentageIdentity_",n,".txt")
+  
+
+  ######################
+  ## WORK STARTS HERE ##
+  ######################
+  
+  # if we're given uniprot IDs then use this for loop to pull the sequences
+  # requires active internet connection
+  if(!is.null(uniprot_IDs)){
+    
+    seqs = c()
+    for(id in uniprot_IDs){
+      ### get the AA sequence from uniprot
+      acc_url = paste0("https://www.uniprot.org/uniprot/",id,".fasta")
+      temp_seq=paste0(read.csv(url(acc_url))[,1],collapse="")
+      
+      seqs[id] = temp_seq
+    }
+    
+
+    
+  } else {
+    seqs = seqs
+  }
+  
+  
+  ## clean up the newlines from input sequences
+  seqs = gsub("[\r\n]", "", seqs)
+  
+  names(seqs) = n_seqs
+  
+  # do the alignment
+  aligned = msa(seqs,order="input",type = "protein")
+  
+  ## calculate percentage identity if only two sequences were aligned
+  if (length(seqs)==2){
+    
+    p_aligned = pairwiseAlignment(seqs[1],seqs[2])
+    perc_id = pid(p_aligned, type="PID1")
+    
+    write.table(perc_id,
+                file=pidFile,
+                quote=F,
+                col.names = F,
+                row.names = F)
+  }
+  
+  
+  
+  # save the alignment in a way texshade can compile
+  msaPrettyPrint(aligned,
+                 alFile = alFile,
+                 output="asis",
+                 askForOverwrite=FALSE)
+  
+  
+  ## save the tex file
+  ### modify the display options as you please!
+  #### easier if you know some latex and requires knowledge of texshade
+  writeLines(c(
+    "\\documentclass[preview]{standalone}",
+    "\\usepackage{texshade}",
+    "\\usepackage{inconsolata}",
+    "\\usepackage{geometry}%[showframe]",
+    
+    "\\begin{document}",
+    paste0("\\begin{texshade}{",alFile,"}"),
+    # IDENTITY HIGHLIGHTING
+    "\\shadingmode[allmatchspecial]{identical}",
+    "\\nomatchresidues{Gray70}{White}{upper}{bf}",
+    "\\conservedresidues{Black}{LightCyan}{upper}{bf}",
+    "\\allmatchresidues{White}{Red}{upper}{bf}",
+    
+    # HIGHLIGHTING THRESHOLD
+    "\\threshold[100]{50}",
+    
+    # LEGEND
+    "\\showlegend",
+    "\\movelegend{0cm}{0cm}",
+    
+    # TOP NUMBERING
+    "\\showruler{1}{top}",
+    #\\hidenumbering
+    
+    # CONSENSUS
+    "\\showconsensus[ColdHot]{bottom}",
+    "\\defconsensus{.}{lower}{upper}",
+    
+    # TEXT SIZE
+    "\\namesfootnotesize",
+    "\\residuesfootnotesize",
+    "\\legendfootnotesize",
+    "\\numberingtiny",
+    
+    # end document
+    "\\end{texshade}",
+    "\\end{document}"),
+    texFile)
+  
+  message("Printing the pdf for you now... \n")
+  
+  ## save pdf in current working directory
+  ## cannot get it to output to base directory directly
+  tools::texi2pdf(texFile, clean=TRUE)
+  #rename the temp file to move it to the desired output folder
+  pdf_file_temp = paste0("~/","alignR_",n,".pdf")
+  file.rename(pdf_file_temp, pdfFile)
+  
+}
+  
+
+  
+
+  
+
+  
+  
+  
+
+
+
